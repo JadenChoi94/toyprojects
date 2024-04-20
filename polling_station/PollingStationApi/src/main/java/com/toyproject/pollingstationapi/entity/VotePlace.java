501e89f238282f742f5357c10a16a6a06b932a1f
jadenchoi94 <jchoi138@student.solbridge.ac.kr>
2024-04-20 22:02:40 +0900

    initial commit

---
+++ polling_station/PollingStationApi/src/main/java/com/toyproject/pollingstationapi/entity/VotePlace.java
@@ -0,0 +1,26 @@
+package com.toyproject.pollingstationapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class VotePlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increase
+    private Long id;
+
+    @Column(nullable = false)
+    private Double posX;
+
+    @Column(nullable = false)
+    private Double posY;
+
+    @Column(nullable = false, length = 100)
+    private String addressKor;
+
+    @Column(nullable = false)
+    private Integer phone;
+}
---
+++ polling_station/PollingStationApi/src/main/java/com/toyproject/pollingstationapi/repository/VotePlaceRepository.java
@@ -0,0 +1,6 @@
+package com.toyproject.pollingstationapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VotePlaceRepository  extends JpaRepository<VotePlace, Long> {
+}
