2b5e686d2568dcad7ca5bc0c63b9c6958a098806
jadenchoi94 <jchoi138@student.solbridge.ac.kr>
2024-04-20 21:06:07 +0900

    set up springboot

---
+++ polling_station/PollingStationApi/src/main/java/com/toyproject/pollingstationapi/PollingStationApiApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.pollingstationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingStationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollingStationApiApplication.class, args);
+    }
+
+}
---
+++ polling_station/PollingStationApi/src/test/java/com/toyproject/pollingstationapi/PollingStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.pollingstationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingStationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
